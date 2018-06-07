e53879170983f2f71fe11353c2788ac35bb62123
Your Name <you@example.com>
2018-06-07 17:00:17 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/phong', function () {
-    return view('phong');
+
+Route::view('/phong', 'phong');
+
+Route::get('user/{id}', function ($id) {
+    switch ($id){
+        case 1:
+            return 'Hello quy';
+        case 2:
+            return 'hello aa';
+        default:
+            return 'hello 00000000';
+    }
 });
+Route::prefix('thoitrang')->group(function () {
+    Route::get('vietnam', function () {
+        return "thoitrangvietnam";
+    });
+    Route::get('quocte', function(){
+        return "thoitrangquocte";
+    });
+});
+Route::get('/', function () {
+    return view('greating', ['name' => 'James']);
+});
+Route::view('/child', 'child');
+Route::view('/contact', 'contact');
+Route::view('/blog-example', 'blog-example');
